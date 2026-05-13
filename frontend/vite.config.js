9e426cabfe0f53816a2e94fdc9df29daec8ef085
asmithanandu <asmithaindrapally@gmail.com>
2026-05-13 23:24:50 +0530

    Fix_frontend_build_issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   envPrefix: ['VITE_', 'TAURI_'],
   build: {
-    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    target: process.env.TAURI_PLATFORM ? (process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13') : 'esnext',
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     sourcemap: !!process.env.TAURI_DEBUG,
   },
